99d4bd94a5dcdd25913e3b7f1b138190c8cce2ee
ngorbunov <nick_g@outlook.com>
2016-05-31 01:41:50 -0500

    add pro mode functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 //declare array for all employees
 var allEmployees = [];
-
 //declare and import variables from the input fields
 var addEmployee = function (){
+
 
   var firstName = document.getElementById('firstNameIn').value;
   var lastName = document.getElementById('lastNameIn').value;
@@ -10,48 +10,53 @@ var addEmployee = function (){
   var title = document.getElementById('jobTitleIn').value;
   var salary = document.getElementById('salaryIn').value;
 
-//create object using declared values
-var employee = {
-  'fname' : firstName,
-  'lname' : lastName,
-  'eidn' : empID,
-  'jtitle' : title,
-  'salary' : salary
-};
+    //create object using declared values
+    var employee = {
+      'fname' : firstName,
+      'lname' : lastName,
+      'eidn' : empID,
+      'jtitle' : title,
+      'sal' : salary
+    };
 
-// console.log(employee);
-//push object into all employees array
-allEmployees.push(employee);
+  //push object into all employees array
+  allEmployees.push(employee);
 
-// console.log(allEmployees);
+  calcSalary();
+  listEmployees();
 
-//clear input fields
+  //clear input fields
+  document.getElementById('firstNameIn').value='';
+  document.getElementById('lastNameIn').value='';
+  document.getElementById('empIDNumIn').value='';
+  document.getElementById('jobTitleIn').value='';
+  document.getElementById('salaryIn').value='';
 
-calcSalary();
-listEmployees();
 };
 //output employees onto DOM
 var listEmployees = function () {
-var outputText = '';
-for(var i=0; i<allEmployees.length; i++){
+  var outputText = '';
+  if (allEmployees.length === 0){ outputText = 'No Employees entered';}
+  else if (allEmployees.length > 0) {
+     for(var i=0; i<allEmployees.length; i++){
+    outputText += 'Name: ' + allEmployees[i].fname +' '+ allEmployees[i].lname +'   EID#: ' + allEmployees[i].eidn +'   Job Title: '+ allEmployees[i].jtitle +'   Salary: ' + allEmployees[i].sal+' <button onClick="removeEmp(' + i + ')">' + 'Remove' + "</button>" + '</p>';
 
-outputText += '<p> Name: ' + allEmployees[i].fname +' '+ allEmployees[i].lname +'   EID#: ' + allEmployees[i].eidn +'   Job Title: '+ allEmployees[i].jtitle +'   Salary: ' + allEmployees[i].salary+'</p>';
-document.getElementById('outputDiv').innerHTML = outputText;
-
-//clear input fields
-document.getElementById('firstNameIn').value='';
-document.getElementById('lastNameIn').value='';
-document.getElementById('empIDNumIn').value='';
-document.getElementById('jobTitleIn').value='';
-document.getElementById('salaryIn').value='';
-}
+  }}
+  document.getElementById('outputDiv').innerHTML ='<p>' + outputText + '</p>';
 };
+
 //calculate total salary expenses
 var calcSalary = function (){
   var totalSalary = 0;
-for(var i=0; i<allEmployees.length; i++){
-totalSalary += parseInt(allEmployees[i].salary);
-//output total salary expenses onto the DOM
-document.getElementById('calculation').innerHTML = 'The current total salary expense is: ' + totalSalary;
-}
+  for(var i=0; i<allEmployees.length; i++){
+    totalSalary += parseInt(allEmployees[i].sal);
+  }
+  document.getElementById('calculation').innerHTML = 'The current total salary expense is: ' + totalSalary;
+};
+//Remove employee from Dom, recalculate salary
+var removeEmp = function (i){
+  allEmployees.splice(i, 1);
+console.log(allEmployees);
+listEmployees();
+calcSalary();
 };
